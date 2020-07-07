b1dffe1f974a49b1b0c93785bd54f30ce9bbe72d
Vitor B Appolinario <vitorappolinario@gmail.com>
2020-07-07 14:46:20 -0300

    Inicia solução Bignumber

---
+++ GetTheLoop/GetTheLoop.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GetTheLoop
+{
+    public class Node : IEquatable<Node>
+    {
+        public Guid Id { get; }
+        public Node Next { get; set; }
+        public Node()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public bool Equals(Node other)
+        {
+            return this.Id.Equals(other.Id);
+        }
+    }
+
+    public class GetTheLoopKata
+    {
+        public static int GetLoopSize(Node startNode)
+        {
+            var fast = startNode.Next.Next;
+            var slow = startNode.Next;
+            while (fast != slow)
+            {
+                slow = slow.Next;
+                fast = fast.Next.Next;
+            }
+
+            fast = fast.Next;
+            int steps = 1;
+            if (!slow.Equals(fast))
+            {
+                fast = fast.Next;
+                steps++;
+            }
+
+            return steps;
+        }
+    }
+}
---
+++ Tests/GetTheLoopTests.cs
@@ -0,0 +1,46 @@
+using NUnit.Framework;
+using GetTheLoop;
+
+namespace Tests
+{
+    public class GetTheLoopTests
+    {
+        [Test, Description("Sample Tests")]
+        public void SampleTest()
+        {
+            var n1 = new Node();
+            var n2 = new Node();
+            var n3 = new Node();
+            var n4 = new Node();
+            n1.Next = n2;
+            n2.Next = n3;
+            n3.Next = n4;
+            n4.Next = n2;
+            Assert.AreEqual(3, GetTheLoopKata.GetLoopSize(n1));
+        }
+
+        [Test, Description("Sample Tests")]
+        public void SampleTest2()
+        {
+            var n1 = new Node();
+            var n2 = new Node();
+            var n3 = new Node();
+            var n4 = new Node();
+            var n5 = new Node();
+            var n6 = new Node();
+            var n7 = new Node();
+            var n8 = new Node();
+            var n9 = new Node();
+            n1.Next = n2;
+            n2.Next = n3;
+            n3.Next = n4;
+            n4.Next = n5;
+            n5.Next = n6;
+            n6.Next = n7;
+            n7.Next = n8;
+            n8.Next = n9;
+            n9.Next = n4;
+            Assert.AreEqual(6, GetTheLoopKata.GetLoopSize(n1));
+        }
+    }
+}
